a37717fc8752660cc8542b5b665ef79fcc69efe4
matteobjornsson <matteobjornsson@gmail.com>
2020-10-11 12:57:20 -0600

    driver pseudocode

---
+++ Driver.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# set arguments such as how many hidden layers, how many nodes per hidden layer
+# identify if the data set is regression, if not, how many classes? 
+# init a neural network with these input parameters
+# chunk training data into batches (?)
+# repeat until error value converges (?)
+#   pass forward and backprop on each batch of input X
+# once Neural network converges, stop training
+# pass in test data on the network and get classification results
+# run the results processing on data (mean squared error, F1, etc)
