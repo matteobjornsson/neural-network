6587c1cafa33b57faa8c0f358ddc7d543190675d
Nicholas Stone <stonen2@xavier.edu>
2020-10-12 18:53:35 -0600

    Adding the Vector Helper class

---
+++ VectorHelper.py
@@ -0,0 +1,27 @@
+
+import numpy as np 
+
+
+
+
+class VectorHelper: 
+    
+
+    def __inti__(self): 
+        print("Vector Helper has been created")
+
+
+    def SoftMax(a): 
+        print(a)
+
+    def CrossEntropy(a): 
+        print(a)
+    
+
+    def Sigmoid(a):
+        print(a)
+
+
+    def SignmoidDerivative(a): 
+        print(a)
+
