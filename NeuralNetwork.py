98ddfdf407c5553e28515f041bbbe5a413e419ff
matteobjornsson <matteobjornsson@gmail.com>
2020-10-24 21:34:11 -0600

    entropy error

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -117,9 +117,9 @@ class NeuralNetwork:
         #Calculate the number of rows in the data set 
         Num_Samples = Estimate.shape[1]
         #S
-        output = self.SoftMax(Ground_Truth)
-        Logrithmic = output * np.log(Estimate + .000000000000001)
-        return np.sum(Logrithmic) / Num_Samples
+        # output = self.SoftMax(Ground_Truth)
+        Logrithmic = Ground_Truth * np.log(Estimate + .000000000000001)
+        return  - np.sum(Logrithmic) / Num_Samples
     
     def SoftMax(self,a): 
         soft = np.exp(a - np.max(a))
