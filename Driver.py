4274be4a884d931be01a2c1720c096a048a65ec9
matteobjornsson <matteobjornsson@gmail.com>
2020-10-26 22:03:02 -0600

    shortened learning

--- Driver.py
+++ Driver.py
@@ -226,7 +226,7 @@ for data_set in data_sets:
         data_set_size = X.shape[1] + test_data.shape[1]
         batch_size = int((data_set_size)/5)
         momentum = 0
-        epochs = 50000
+        epochs = 5000
         learning_rate = .0001
 
         hidden_layers = []
