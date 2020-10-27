e7d0af2e3f28cb018907a16ab5dc32ba386e31b5
matteobjornsson <matteobjornsson@gmail.com>
2020-10-26 23:07:15 -0600

    parallel tuning

--- Driver.py
+++ Driver.py
@@ -8,6 +8,7 @@ import NeuralNetwork
 import matplotlib.pyplot as plt
 import time 
 import Performance
+import multiprocessing
 
 # set arguments such as how many hidden layers, how many nodes per hidden layer
 # identify if the data set is regression, if not, how many classes? 
@@ -41,8 +42,8 @@ def batch_input_data(X: np.ndarray, labels: np.ndarray, batch_size: int) -> list
         batches.append([X_i, labels_i])
     return batches
 
-def driver(input_size_d, hidden_layers_d, regression_d, output_size_d, learning_rate_d, momentum_d,
-            X_d, labels_d, batch_size_d, epochs_d, test_data_d, test_labels_d, counter):
+def driver(q, input_size_d, hidden_layers_d, regression_d, output_size_d, learning_rate_d, momentum_d,
+            X_d, labels_d, batch_size_d, epochs_d, test_data_d, test_labels_d):
 
     NN = NeuralNetwork.NeuralNetwork(
         input_size_d, hidden_layers_d, regression_d, output_size_d, learning_rate_d, momentum_d
@@ -50,32 +51,29 @@ def driver(input_size_d, hidden_layers_d, regression_d, output_size_d, learning_
     # print("shape x", X.shape)
 
     # print(vars(NN))
-    print(f"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ { data_set } $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n")
-    print("total cycles: ", math.ceil(X_d.shape[1]/batch_size_d)*epochs_d, "batches:",int(X_d.shape[1]/batch_size_d), "batch size:", batch_size_d, "learning rate:", learning_rate_d)
-    plt.ion()
+    # print(f"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ { data_set } $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n")
+    # print("total cycles: ", math.ceil(X_d.shape[1]/batch_size_d)*epochs_d, "batches:",int(X_d.shape[1]/batch_size_d), "batch size:", batch_size_d, "learning rate:", learning_rate_d)
     batches = batch_input_data(X_d, labels_d, batch_size_d)
-    for i in range(epochs):
-        
+    for i in range(epochs_d):
         for batch in batches:
             X_i = batch[0]
             labels_i = batch[1]
             NN.set_input_data(X_i, labels_i)
             NN.forward_pass()
             NN.backpropagation_pass()
-        if i % 100 == 0:
-            if counter < 2:
-                plt.plot(NN.error_x, NN.error_y)
-                plt.draw()
-                plt.pause(0.00001)
-                plt.clf()
-    if counter == 1:
-        plt.ioff()
-        plt.plot(NN.error_x, NN.error_y)
-        plt.show()
-        print("\n Labels: \n",labels)
 
+        # if i % 100 == 0:
+        #     if counter < 2:
+        #         plt.plot(NN.error_x, NN.error_y)
+        #         plt.draw()
+        #         plt.pause(0.00001)
+        #         plt.clf()
+    # if counter == 1:
+    #     plt.ioff()
+    #     plt.plot(NN.error_x, NN.error_y)
+    #     plt.show()
+    #     print("\n Labels: \n",labels)
 
-    
     Estimation_Values = NN.classify(test_data_d,test_labels_d)
     if regression_d == False: 
         #Decode the One Hot encoding Value 
@@ -110,7 +108,6 @@ def driver(input_size_d, hidden_layers_d, regression_d, output_size_d, learning_
     batch_size = 20
     epochs = 500
     """
-    Meta = list()
     #Meta Data order
     h1 = 0 
     h2 = 0 
@@ -131,26 +128,23 @@ def driver(input_size_d, hidden_layers_d, regression_d, output_size_d, learning_
         h1 = hidden_layers_d[0]
         #The number of nodes per hidden layer 
         h2 = hidden_layers_d[1]
-    #The number of hidden layers 
-    Meta.append(data_set)
-    #The number of hidden layers
-    Meta.append(len(hidden_layers_d))
-    #Number of nodes in h1 
-    Meta.append(h1)
-    #Number of Nodes in h2 
-    Meta.append(h2)
-    #Learning Rate
-    Meta.append(learning_rate_d) 
-    #Momentum 
-    Meta.append(momentum_d)
-    #Batch Size 
-    Meta.append(batch_size_d)
-    #number of batches
-    Meta.append(len(batches))
-    #Epochs
-    Meta.append(epochs_d)
-    Per.StartLossFunction(regression_d,Nice,Meta,filename)
 
+    #[data set, number of h layers, node per h 1, nodes per h2, learning rate, momentum, batch size, number batches, number epochs]
+    Meta = [data_set, len(hidden_layers_d), h1, h2, learning_rate_d, momentum_d, batch_size_d, len(batches), epochs_d]
+    results_set = Per.LossFunctionPerformance(regression_d,Nice)
+    data_point = Meta + results_set
+    data_point_string = ','.join([str(x) for x in data_point])
+    # put the result on the multiprocessing queue
+    q.put(data_point_string)
+
+def data_writer(q, filename):
+    while True:
+        with open(filename, 'a') as f:
+            data_string = q.get()
+            if data_string == 'kill':
+                f.write('\n')
+                break
+            f.write(data_string + '\n')
 
 data_sets = ["abalone","Cancer","glass","forestfires","soybean","machine"] 
 
@@ -177,112 +171,134 @@ Per = Performance.Results()
 Per.PipeToFile([], headers, filename)
 
 
-
 for data_set in data_sets:
     if data_set != "soybean": continue
-    tuning_pass = 0
+
+    manager = multiprocessing.Manager()
+    q = manager.Queue()
+    start = time.time()
+    writer = multiprocessing.Process(target=data_writer, args=(q,filename))
+    writer.start()
+
+    pool = multiprocessing.Pool()
+
     du = DataUtility.DataUtility(categorical_attribute_indices, regression_data_set)
     # ten fold data and labels is a list of [data, labels] pairs, where 
     # data and labels are numpy arrays:
     tenfold_data_and_labels = du.Dataset_and_Labels(data_set)
+    test_data, test_labels = copy.deepcopy(tenfold_data_and_labels[0])
+    #Append all data folds to the training data set
+    remaining_data = [x[0] for i, x in enumerate(tenfold_data_and_labels) if i!=0]
+    remaining_labels = [y[1] for i, y in enumerate(tenfold_data_and_labels) if i!=0]
+    #Store off a set of the remaining dataset 
+    X = np.concatenate(remaining_data, axis=1) 
+    #Store the remaining data set labels 
+    labels = np.concatenate(remaining_labels, axis=1)
+    regression = regression_data_set[data_set]
+    #If the data set is a regression dataset
+    if regression == True:
+        #The number of output nodes is 1 
+        output_size = 1
+    #else it is a classification data set 
+    else:
+        #Count the number of classes in the label data set 
+        output_size = du.CountClasses(labels)
+        #Get the test data labels in one hot encoding 
+        test_labels = du.ConvertLabels(test_labels, output_size)
+        #Get the Labels into a One hot encoding 
+        labels = du.ConvertLabels(labels, output_size)
+    input_size = X.shape[0]
 
-    # execute driver for each of the ten folds
-    for j in range(10):
-        if j > 2: continue
+    data_set_size = X.shape[1] + test_data.shape[1]
+    momentum = 0
 
-        test_data, test_labels = copy.deepcopy(tenfold_data_and_labels[j])
-        #Append all data folds to the training data set
-        remaining_data = [x[0] for i, x in enumerate(tenfold_data_and_labels) if i!=j]
-        remaining_labels = [y[1] for i, y in enumerate(tenfold_data_and_labels) if i!=j]
-        #Store off a set of the remaining dataset 
-        X = np.concatenate(remaining_data, axis=1) 
-        #Store the remaining data set labels 
-        labels = np.concatenate(remaining_labels, axis=1)
-        # #Test data print to the scren 
-        # print("data:", X.shape, '\n', X)
-        # print()
-        # print("labels:", labels.shape, '\n', labels)
-        regression = regression_data_set[data_set]
-        #If the data set is a regression dataset
-        if regression == True:
-            #The number of output nodes is 1 
-            output_size = 1
-        #else it is a classification data set 
-        else:
-            #Count the number of classes in the label data set 
-            output_size = du.CountClasses(labels)
-            #Get the test data labels in one hot encoding 
-            test_labels = du.ConvertLabels(test_labels, output_size)
-            #Get the Labels into a One hot encoding 
-            labels = du.ConvertLabels(labels, output_size)
+    learning_rates = [.1, .01, .001, .001, .0001, .00001]
+    epochs = [5000, 50000, 500000]
+    batch_counts = [2, 5, 10, 20, 50]
+    hidden_layers = [[], [int(input_size/2)], [int(input_size/2),int(input_size/2)]]
 
-        #Print the labels meta data to the screen and the label data set 
-        # print("labels:", labels.shape, '\n', labels)
-        # print("Test labels:", test_labels.shape, '\n', test_labels)
-        input_size = X.shape[0]
-        
-
-        ############# hyperparameters ################
-        
-        
-        data_set_size = X.shape[1] + test_data.shape[1]
-        batch_size = int((data_set_size)/3) # or just 100?
-        momentum = 0
-        epochs = 10000
-        learning_rate = .001
-
-        hidden_layers = []
-        driver(input_size_d=input_size,
-            hidden_layers_d=hidden_layers,
-            regression_d=regression,
-            output_size_d=output_size,
-            learning_rate_d=learning_rate,
-            momentum_d=momentum,
-            X_d=X,
-            labels_d=labels,
-            batch_size_d=batch_size,
-            epochs_d=epochs,
-            test_data_d=test_data,
-            test_labels_d=test_labels,
-            counter=tuning_pass)
-
-        tuning_h1 = [m for m in reversed(range(1, input_size + 1, int(input_size/4)))]
+    for h in hidden_layers:
+        for e in epochs:
+            for lr in learning_rates:
+                for bc in batch_counts:
+                    batch_size = int((data_set_size)/bc)
+                    pool.apply_async(driver, args=(
+                        q, 
+                        input_size,
+                        h,
+                        regression,
+                        output_size,
+                        lr,
+                        momentum,
+                        X,
+                        labels,
+                        bc,
+                        e,
+                        test_data,
+                        test_labels,
+                        )
+                    )
+                break
     
-        for hidden_1 in tuning_h1:
-            hidden_layers = [hidden_1]
-            ##############################################
+    pool.close()
+    pool.join()
+    q.put('kill')
+    writer.join()
+    elapsed_time = time.time() - start
+    print("Elapsed time: ", elapsed_time, 's')
 
-            driver(input_size_d=input_size,
-            hidden_layers_d=hidden_layers,
-            regression_d=regression,
-            output_size_d=output_size,
-            learning_rate_d=learning_rate,
-            momentum_d=momentum,
-            X_d=X,
-            labels_d=labels,
-            batch_size_d=batch_size,
-            epochs_d=epochs,
-            test_data_d=test_data,
-            test_labels_d=test_labels,
-            counter=tuning_pass)
+        # hidden_layers = []
+        # driver(input_size_d=input_size,
+        #     hidden_layers_d=hidden_layers,
+        #     regression_d=regression,
+        #     output_size_d=output_size,
+        #     learning_rate_d=learning_rate,
+        #     momentum_d=momentum,
+        #     X_d=X,
+        #     labels_d=labels,
+        #     batch_size_d=batch_size,
+        #     epochs_d=epochs,
+        #     test_data_d=test_data,
+        #     test_labels_d=test_labels,
+        #     counter=tuning_pass)
+
+        # tuning_h1 = [m for m in reversed(range(1, input_size + 1, int(input_size/4)))]
+    
+        # for hidden_1 in tuning_h1:
+        #     hidden_layers = [hidden_1]
+        #     ##############################################
+
+        #     driver(input_size_d=input_size,
+        #     hidden_layers_d=hidden_layers,
+        #     regression_d=regression,
+        #     output_size_d=output_size,
+        #     learning_rate_d=learning_rate,
+        #     momentum_d=momentum,
+        #     X_d=X,
+        #     labels_d=labels,
+        #     batch_size_d=batch_size,
+        #     epochs_d=epochs,
+        #     test_data_d=test_data,
+        #     test_labels_d=test_labels,
+        #     counter=tuning_pass)
         
-        for hidden_1 in tuning_h1:
-            hidden_layers = [hidden_1, hidden_1]
-            ##############################################
+        # for hidden_1 in tuning_h1:
+        #     hidden_layers = [hidden_1, hidden_1]
+        #     ##############################################
 
-            driver(input_size_d=input_size,
-            hidden_layers_d=hidden_layers,
-            regression_d=regression,
-            output_size_d=output_size,
-            learning_rate_d=learning_rate,
-            momentum_d=momentum,
-            X_d=X,
-            labels_d=labels,
-            batch_size_d=batch_size,
-            epochs_d=epochs,
-            test_data_d=test_data,
-            test_labels_d=test_labels,
-            counter=tuning_pass)
+        #     driver(input_size_d=input_size,
+        #     hidden_layers_d=hidden_layers,
+        #     regression_d=regression,
+        #     output_size_d=output_size,
+        #     learning_rate_d=learning_rate,
+        #     momentum_d=momentum,
+        #     X_d=X,
+        #     labels_d=labels,
+        #     batch_size_d=batch_size,
+        #     epochs_d=epochs,
+        #     test_data_d=test_data,
+        #     test_labels_d=test_labels,
+        #     counter=tuning_pass)
 
-            tuning_pass += 1
+        #     tuning_pass += 1
         
